1fc1734d981e863ee62b1650bdb70a83a37994e7
Libbie Tammie <ltammie@vo-g3.msk.21-school.ru>
2020-10-25 17:51:25 +0300

    g

--- TanksClient/src/main/java/tanks/windows/ClientWindow.java
+++ TanksClient/src/main/java/tanks/windows/ClientWindow.java
@@ -74,12 +74,9 @@ public class ClientWindow {
                     }
                 });
 
-        int newPos = 360;
-        int enemyPos = 360;
 
         gc.drawImage(field, 0, 0, canvas.getWidth(), canvas.getHeight());
-        gc.drawImage(tank, newPos, 650, 80, 100);
-        gc.drawImage(enemy, enemyPos, 50, 80, 100);
+        gc.drawImage(tank, 360, 650, 80, 100);
         primaryStage.show();
 //
         while (!in.readBoolean()) ;
@@ -87,10 +84,13 @@ public class ClientWindow {
 
         final long startNanoTime = System.nanoTime();
 
-//        new AnimationTimer() {
-//            public void handle(long currentNanoTime) {
-//                    double t = (currentNanoTime - startNanoTime) / 1000000000.0;
-        while (true) {
+        new AnimationTimer() {
+            public void handle(long currentNanoTime) {
+                    double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+//        while (true) {
+
+                int newPos;
+                int enemyPos = 360;
                 newPos = 360 + shift.get();
 
                 try {
@@ -110,9 +110,8 @@ public class ClientWindow {
                 gc.drawImage(field, 0, 0, canvas.getWidth(), canvas.getHeight());
                 gc.drawImage(tank, newPos, 650, 80, 100);
                 gc.drawImage(enemy, enemyPos, 50, 80, 100);
-                primaryStage.show();
         }
-//        }.start();
+        }.start();
     }
 
     private static void stopThread() {
