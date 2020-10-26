fe3798cef07e3e2792cc1dbcb38d5738b5b20e3c
Libbie Tammie <ltammie@vo-f1.msk.21-school.ru>
2020-10-26 10:37:02 +0300

    g

--- TanksClient/src/main/java/tanks/windows/ClientWindow.java
+++ TanksClient/src/main/java/tanks/windows/ClientWindow.java
@@ -124,14 +124,12 @@ public class ClientWindow {
                     for (int i = 0; i < b; i++){
                         int x = in.readInt();
                         int y = in.readInt();
-                        System.out.println("p " + x + " " + y);
                         gc.drawImage(bullet, x, y, 10, 10);
                     }
                     eb = in.readInt();
                     for (int i = 0; i < eb; i++){
                         int x = in.readInt();
                         int y = in.readInt();
-                        System.out.println("e " + x + " " + y);
                         gc.drawImage(eBullet, x, y, 10, 10);
                     }
                 } catch (IOException e) {
