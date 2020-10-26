d98d2b10c898fbf81a809176a97fe4ca7f58ac13
Libbie Tammie <ltammie@vo-f1.msk.21-school.ru>
2020-10-26 11:03:04 +0300

    g

--- TanksClient/src/main/java/tanks/windows/ClientWindow.java
+++ TanksClient/src/main/java/tanks/windows/ClientWindow.java
@@ -181,7 +181,7 @@ public class ClientWindow {
             dialogVbox.getChildren().add(new Text("This is a Dialog"));
             Scene dialogScene = new Scene(dialogVbox, 300, 200);
             dialog.setScene(dialogScene);
-            dialog.show();
+            dialog.showAndWait();
         }
 
     }
